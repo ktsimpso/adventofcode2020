1758d21b9921a11b42353601316d5a7c036714f5
Kevin Simpson <ktsimpso@gmail.com>
2020-12-15 22:18:15 -0800

    Completed day12 part2

--- src/rain_risk.rs
+++ src/rain_risk.rs
@@ -1,16 +1,26 @@
 use crate::lib::{default_sub_command, file_to_lines, parse_isize, parse_lines, Command};
 use anyhow::Error;
-use clap::{value_t_or_exit, App, ArgMatches, SubCommand};
+use clap::{value_t_or_exit, App, Arg, ArgMatches, SubCommand};
 use nom::{character::complete, combinator::map_res, sequence::tuple};
 use simple_error::SimpleError;
 use std::collections::HashMap;
 use std::convert::TryFrom;
+use strum::VariantNames;
+use strum_macros::{EnumString, EnumVariantNames};
 
 pub const RAIN_RISK: Command = Command::new(sub_command, "rain-risk", run);
 
 #[derive(Debug)]
 struct RainRiskArgs {
     file: String,
+    direction_strategy: DirectionStrategy,
+}
+
+#[derive(Debug, EnumString, EnumVariantNames)]
+#[strum(serialize_all = "kebab_case")]
+enum DirectionStrategy {
+    Relative,
+    Waypoint,
 }
 
 #[derive(Debug, Clone, PartialEq, Copy, Eq, Hash)]
@@ -27,15 +37,32 @@ enum Direction {
 fn sub_command() -> App<'static, 'static> {
     default_sub_command(
         &RAIN_RISK,
-        "Takes a file with a seating chart and finds stats about people sitting",
-        "Path to the input file. Each line contains one row of seats.",
+        "Takes a file with directions and returns the manhatten distance the ship moved.",
+        "Path to the input file. Each line contains one direction instruction",
+    )
+    .arg(
+        Arg::with_name("direction-strategy")
+            .short("d")
+            .help(
+                "How to interperate the direction instructions. The possible value are as follows:\n\n\
+            relative: Directions are relative to the ship.\n\n\
+            waypoint: Directions are relayove to a waypoint.\n",
+            )
+            .takes_value(true)
+            .possible_values(&DirectionStrategy::VARIANTS)
+            .required(true),
     )
     .subcommand(
         SubCommand::with_name("part1")
             .about(
-                "Finds an equalibrium for steating arrangements with a tolerance of 4, and \
-                adjacency of directly-next-to and then returns the number of occupied seats \
-                with the default input.",
+                "Finds the Manhattan distance using relative direction-strategy and the default intput.",
+            )
+            .version("1.0.0"),
+    )
+    .subcommand(
+        SubCommand::with_name("part2")
+            .about(
+                "Finds the Manhattan distance using waypoint direction-strategy and the default intput.",
             )
             .version("1.0.0"),
     )
@@ -45,9 +72,18 @@ fn run(arguments: &ArgMatches) -> Result<(), Error> {
     let rain_risk_arguments = match arguments.subcommand_name() {
         Some("part1") => RainRiskArgs {
             file: "day12/input.txt".to_string(),
+            direction_strategy: DirectionStrategy::Relative,
+        },
+        Some("part2") => RainRiskArgs {
+            file: "day12/input.txt".to_string(),
+            direction_strategy: DirectionStrategy::Waypoint,
         },
         _ => RainRiskArgs {
             file: value_t_or_exit!(arguments.value_of("file"), String),
+            direction_strategy: value_t_or_exit!(
+                arguments.value_of("direction-strategy"),
+                DirectionStrategy
+            ),
         },
     };
 
@@ -62,7 +98,10 @@ fn process_directions(rain_risk_arguments: &RainRiskArgs) -> Result<isize, Error
     file_to_lines(&rain_risk_arguments.file)
         .and_then(|lines| parse_lines(lines, parse_directions))
         .map(|directions| {
-            let (x, y) = travel_directions(&directions);
+            let (x, y) = match rain_risk_arguments.direction_strategy {
+                DirectionStrategy::Relative => travel_directions(&directions),
+                DirectionStrategy::Waypoint => travel_directions_waypoint(&directions),
+            };
             x.abs() + y.abs()
         })
 }
@@ -70,21 +109,7 @@ fn process_directions(rain_risk_arguments: &RainRiskArgs) -> Result<isize, Error
 fn travel_directions(directions: &Vec<Direction>) -> (isize, isize) {
     let mut point = (0, 0);
     let mut current_direction = Direction::East(0);
-    let index_direction: HashMap<usize, Direction> = [
-        Direction::North(0),
-        Direction::East(0),
-        Direction::South(0),
-        Direction::West(0),
-    ]
-    .iter()
-    .enumerate()
-    .map(|(index, direction)| (index, *direction))
-    .collect();
-    let direction_index: HashMap<Direction, usize> = index_direction
-        .clone()
-        .into_iter()
-        .map(|(index, direction)| (direction, index))
-        .collect();
+    let (index_direction, direction_index) = generate_index_directions();
 
     for direction in directions {
         match direction {
@@ -113,6 +138,66 @@ fn travel_directions(directions: &Vec<Direction>) -> (isize, isize) {
     }
 
     point
+}
+
+fn travel_directions_waypoint(directions: &Vec<Direction>) -> (isize, isize) {
+    let mut point = (0, 0);
+    let mut waypoint = (1, 10);
+
+    for direction in directions {
+        match direction {
+            Direction::North(x) => waypoint.0 += x,
+            Direction::East(x) => waypoint.1 += x,
+            Direction::South(x) => waypoint.0 -= x,
+            Direction::West(x) => waypoint.1 -= x,
+            Direction::Right(x) => {
+                let steps = x / 90;
+                for _ in 0..steps {
+                    waypoint = rotate_waypoint_right_once(&waypoint);
+                }
+            }
+            Direction::Left(x) => {
+                let steps = x / 90;
+                for _ in 0..steps {
+                    waypoint = rotate_waypoint_left_once(&waypoint);
+                }
+            }
+            Direction::Forward(x) => {
+                point.0 += waypoint.0 * x;
+                point.1 += waypoint.1 * x
+            }
+        }
+    }
+
+    point
+}
+
+fn rotate_waypoint_right_once(waypoint: &(isize, isize)) -> (isize, isize) {
+    (-waypoint.1, waypoint.0)
+}
+
+fn rotate_waypoint_left_once(waypoint: &(isize, isize)) -> (isize, isize) {
+    (waypoint.1, -waypoint.0)
+}
+
+fn generate_index_directions() -> (HashMap<usize, Direction>, HashMap<Direction, usize>) {
+    let index_direction: HashMap<usize, Direction> = [
+        Direction::North(0),
+        Direction::East(0),
+        Direction::South(0),
+        Direction::West(0),
+    ]
+    .iter()
+    .enumerate()
+    .map(|(index, direction)| (index, *direction))
+    .collect();
+    let direction_index: HashMap<Direction, usize> = index_direction
+        .clone()
+        .into_iter()
+        .map(|(index, direction)| (direction, index))
+        .collect();
+
+    (index_direction, direction_index)
 }
 
 fn parse_directions(line: &String) -> Result<Direction, Error> {
